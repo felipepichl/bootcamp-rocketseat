4cc1f903e31e3fc11b08f72518c16251797cbfb0
Felipe Pichl <felipe.pichl@hotmail.com>
2020-07-14 18:35:25 -0300

    Level 05 - GoBarber Front-end - Next appointment

--- GoBarber/frontend/src/pages/Dashboard/index.tsx
+++ GoBarber/frontend/src/pages/Dashboard/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const { signOut, user } = useAuth();
-  console.log(user);
 
   return (
     <Container>
@@ -30,6 +38,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/22602639?s=460&u=2c1bec46e256602d1dd5f173b152233fc58f2855&v=4"
+                alt="Felipe Pichl"
+              />
+
+              <strong>Felipe Pichl</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+          <Calendar />
+        </Schedule>
+      </Content>
     </Container>
   );
 };
