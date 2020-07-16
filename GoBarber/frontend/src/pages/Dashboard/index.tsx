addd44bd900a59af5565160b7e2052a74929f6c2
Felipe <felipe.pichl@hotmail.com>
2020-07-16 16:43:44 -0300

    Level 05: GoBarber Front-end: Month availability

--- GoBarber/frontend/src/pages/Dashboard/index.tsx
+++ GoBarber/frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect, useMemo } from 'react';
 import DayPicker, { DayModifiers } from 'react-day-picker';
-import 'react-day-picker/lib/style.css'
+import 'react-day-picker/lib/style.css';
 
 import { FiPower, FiClock } from 'react-icons/fi';
+import { date } from 'yup';
 import {
   Container,
   Header,
@@ -18,19 +19,58 @@ import {
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
-import { date } from 'yup';
+import api from '../../services/api';
+
+interface MonthAvailabilityItem {
+  day: number;
+  available: boolean;
+}
 
 const Dashboard: React.FC = () => {
-  const [seletedDate, setSelectedDate] = useState(new Date());
-
-  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers ) => {
-    if(modifiers.available) {
-      setSelectedDate(day)
-    }
-
-  }, [])
-
   const { signOut, user } = useAuth();
+
+  const [seletedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const [monthAvailability, setMonthAvailability] = useState<
+    MonthAvailabilityItem[]
+  >([]);
+
+  const handleDayChange = useCallback((day: Date, modifiers: DayModifiers) => {
+    if (modifiers.available) {
+      setSelectedDate(day);
+    }
+  }, []);
+
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month);
+  }, []);
+
+  useEffect(() => {
+    api
+      .post(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      })
+      .then((response) => {
+        setMonthAvailability(response.data);
+      });
+  }, [currentMonth, user.id]);
+
+  const disableDays = useMemo(() => {
+    const dates = monthAvailability
+      .filter((monthDay) => monthDay.available === false)
+      .map((monthDay) => {
+        const year = currentMonth.getFullYear();
+        const month = currentMonth.getMonth();
+
+        return new Date(year, month, monthDay.day);
+      });
+
+    return dates;
+  }, [currentMonth, monthAvailability]);
 
   return (
     <Container>
@@ -39,7 +79,14 @@ const Dashboard: React.FC = () => {
           <img src={logoImg} alt="" />
 
           <Profile>
-            <img src={user.avatar_url} alt={user.name} />
+            <img
+              src={
+                user.avatar_url
+                  ? user.avatar_url
+                  : 'https://api.adorable.io/avatars/60/abott@adorable.png'
+              }
+              alt={user.name}
+            />
 
             <div>
               <span>Bem-Vindo,</span>
@@ -138,12 +185,11 @@ const Dashboard: React.FC = () => {
           <DayPicker
             weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
             fromMonth={new Date()}
-            disabledDays={[
-              { daysOfWeek: [0, 6] }
-            ]}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disableDays]}
             modifiers={{
-              available: { daysOfWeek: [1, 2, 3, 4, 5] }
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
             }}
+            onMonthChange={handleMonthChange}
             selectedDays={seletedDate}
             onDayClick={handleDayChange}
             months={[
