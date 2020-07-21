13a17213ca24162b4212917fe04b0829b640078a
Felipe <felipe.pichl@hotmail.com>
2020-07-21 13:56:13 -0300

    Level 05: GoBarber Mobile: List providers of API

--- GoBarber/mobile/src/hooks/auth.tsx
+++ GoBarber/mobile/src/hooks/auth.tsx
@@ -46,6 +46,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (user[1] && token[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -67,6 +69,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
