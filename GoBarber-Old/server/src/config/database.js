d5d617bd9fb0ea28517c292c32f335908e82d73e
Felipe <felipe.pichl@hotmail.com>
2021-02-03 16:23:33 -0300

    GoBarber-Old - Sequelize settings

---
+++ GoBarber-Old/server/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '186.216.250.224',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gobarber_old',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
