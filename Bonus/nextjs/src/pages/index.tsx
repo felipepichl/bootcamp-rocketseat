1ce934d29d20aafe58e09eea6da3ff05f432966d
Felipe Pichl <felipe.pichl@hotmail.com>
2021-03-05 16:42:52 -0300

    Bonus - NextJS - Client Side Fetching

--- Bonus/nextjs/src/pages/index.tsx
+++ Bonus/nextjs/src/pages/index.tsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Title } from '../styles/pages/Home'
 
+interface IProducts {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProducts[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    })
+  }, [])
+
   return (
     <div>
-      <Title>Hello World</Title>
+      <Title>Products</Title>
+      <ul>
+        {
+          recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
