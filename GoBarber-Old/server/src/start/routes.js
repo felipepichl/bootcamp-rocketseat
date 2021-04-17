ccffee9aeaa6303b635514c5d978be37ad53a1d5
Felipe Pichl <felipe.pichl@hotmail.com>
2021-04-16 21:38:31 -0300

    GoBarber-Old - Schedule provider

--- GoBarber-Old/server/src/start/routes.js
+++ GoBarber-Old/server/src/start/routes.js
@@ -8,6 +8,7 @@ import UserController from '../app/controllers/UserController';
 import ProviderController from '../app/controllers/ProviderController';
 import FileController from '../app/controllers/FileController';
 import AppointmentController from '../app/controllers/AppointmentController';
+import ScheduleController from '../app/controllers/ScheduleController';
 
 import authMiddleware from '../app/middlewares/auth';
 
@@ -23,9 +24,11 @@ routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
